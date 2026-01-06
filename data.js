384c573e510577a9c5c81afaeeee192205953f01
DucAnhCao11 <ducanhcao2611@gmail.com>
2026-01-06 16:42:03 +0700

    initial commit

---
+++ data.js
@@ -0,0 +1,43 @@
+const data_name = [
+    {
+        ma_ten: "thien_trang",
+        name: "&#129505Gửi Thiên Trang !",
+        message: "Chúc em một năm mới thật nhiều bình yên trong tim,\n" +
+                "mỗi buổi sáng thức dậy đều có lý do để mỉm cười,\n" +
+                "mỗi buổi tối đi ngủ đều thấy lòng nhẹ nhàng vì đã cố gắng hết mình.\n" +
+
+                "Mong rằng những ngày mệt mỏi sẽ sớm qua,\n" +
+                "những ước mơ em ấp ủ sẽ dần thành hiện thực,\n" +
+                "và dù cuộc sống có thế nào đi nữa,\n" +
+                "em vẫn luôn cảm nhận được rằng có một người âm thầm quan tâm và thương em rất nhiều.\n\n" +
+
+                "Chúc em luôn là chính em – dịu dàng, mạnh mẽ và đáng yêu theo cách riêng.",
+        img: "image/thien_trang.jpg"
+    },
+    {
+        ma_ten: "nghia",
+        name: "&#129505Gửi Bạn tôi !",
+        message: "Chúc mừng năm mới, bạn của tôi!\n" +
+                "Dù đã lâu kể từ lần cuối chúng ta gặp nhau," +
+                "nhưng tình bạn của chúng ta vẫn luôn sống động trong tim tôi.\n" +
+                "Cảm ơn bạn vì tất cả những gì bạn đã làm — từ những lúc tôi gặp khó khăn trong quá khứ, \n" +
+                "đến những khoảnh khắc hiện tại và cả những ngày sắp tới," +
+                "bạn luôn sẵn sàng bên tôi, giúp đỡ mà không hề toan tính.\n" +
+                "Mong rằng năm mới này sẽ mang đến cho bạn thật nhiều niềm vui, bình yên và sức khỏe, và muôn vàn may mắn.\n" +
+                "Và hy vọng, một ngày không xa, chúng ta sẽ được gặp nhau và kể cho nhau nghe tất cả những chuyện đã trải qua.",
+        img: "image/nghia.jpg"
+    },
+    {
+        ma_ten: "chi_thanh",
+        name: "&#129505Gửi Chị gái !",
+        message: "Nhân dịp năm mới, em chỉ muốn nói với chị một điều từ tận đáy lòng: " +
+                "em biết ơn chị rất nhiều. Cảm ơn chị vì đã luôn là người đứng sau, âm thầm lo cổ vũ," +
+                "tạo động lực, để em có thể yên tâm học hành và trưởng thành.\n" +
+                "Có những lúc em chưa nói ra được hết, nhưng trong lòng em luôn khắc ghi sự hy sinh và tình thương của chị.\n" +
+                "Năm mới đến rồi, em chỉ mong gđ chị luôn mạnh khỏe, bình an, và có thật nhiều niềm vui trong cuộc sống." +
+                "Em hứa sẽ cố gắng sống tốt hơn, học tập chăm chỉ hơn, " +
+                "để một ngày nào đó chị có thể tự hào và yên tâm rằng những gì chị dành cho em là hoàn toàn xứng đáng.\n" +
+                "Chúc mừng năm mới chị yêu quý của em!",
+        img: "image/chi_thanh.jpg"
+    }
+]
