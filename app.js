70ce62dde72b46d69eaf507a496473c8b29c0807
DucAnhCao11 <ducanhcao2611@gmail.com>
2026-01-06 17:05:57 +0700

    fix: update time year

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var fut = new Date("jan 06, 2026 15:46:00").getTime()
+var fut = new Date("Feb 16, 2026 23:59:59").getTime()
 let x = setInterval(function stime(){
     var now = new Date().getTime()
     var D = fut - now
